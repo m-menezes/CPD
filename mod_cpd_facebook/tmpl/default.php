050aa510408612069fea3371c9d21af2e1ac05cc
m3nezes <m3nezes@gmail.com>
2017-06-22 15:10:26 -0300

    update mod_facebook

--- mod_cpd_facebook/tmpl/default.php
+++ mod_cpd_facebook/tmpl/default.php
@@ -7,10 +7,32 @@
 
 
 defined('_JEXEC') or die; ?>
-<div class="col-md-12 nopad" style="border:1px solid #CCC; margin-bottom: 10px;">
-	<div class="fb-page" data-href="<?php echo $facebookURL ?>" data-tabs="<?php echo $publicacoes ?>" data-width="<?php echo $dataWidth ?>" data-height="<?php echo $dataHeight ?>" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-		<blockquote cite="<?php echo $facebookURL ?>" class="fb-xfbml-parse-ignore">
-			<a href="https://www.facebook.com/facebook">Facebook</a>
-		</blockquote>
+<div class="col-md-12 nopad">
+	<div class="textVisual">
+		<i class="fa fa-facebook-official" aria-hidden="true" style="float:left;"></i>
+		<p class="visualTexto">
+			Facebook
+		</p>
+	</div>
+	<div class="col-md-12 nopad" style="border:1px solid #CCC; margin-bottom: 10px;">
+		<div class="fb-page" data-href="<?php echo $facebookURL ?>" data-tabs="<?php echo $publicacoes ?>" data-width="<?php echo $dataWidth ?>" data-height="<?php echo $dataHeight ?>" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+			<blockquote cite="<?php echo $facebookURL ?>" class="fb-xfbml-parse-ignore">
+				<a href="https://www.facebook.com/facebook">Facebook</a>
+			</blockquote>
+		</div>
 	</div>
 </div>
+<style type="text/css">
+	.textVisual p{
+		text-transform: uppercase;
+		color:white;
+		padding: 3px;
+	}
+	.textVisual i{
+		color:white;
+		padding: 5px;
+	}
+	.visualTexto{
+		color:white;
+	}
+</style>
